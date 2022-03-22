92c86b0a4334580fb5010a78701d566b5c735080
Nicolas Ballarini <nicoballarini@gmail.com>
2022-03-22 13:10:05 +0000

    version 1.0.1

--- R/get_prca_data.R
+++ R/get_prca_data.R
@@ -1,9 +1,15 @@
 get_prca_data <- function() {
   # data_url = "https://www.uniklinik-freiburg.de/fileadmin/mediapool/08_institute/biometrie-statistik/pdf/Studium_und_Lehre/Lehrbuecher/Multivariable_Model-building/adv_prostate_ca.zip"
   data_url = "https://www.uniklinik-freiburg.de/fileadmin/mediapool/08_institute/biometrie-statistik/Dateien/Studium_und_Lehre/Lehrbuecher/Multivariable_Model-building/adv_prostate_ca.zip"
-  temp <- tempfile()
-  utils::download.file(data_url, temp, cacheOK = FALSE)
-  prca = utils::read.csv(unz(temp, "adv_prostate_ca/adv_prostate_ca.csv"))
-  names(prca) = toupper(names(prca))
-  prca
+  if (httr::http_error(data_url)) { # network is down = message (not an error)
+    message("No internet connection or data source broken.")
+    return(NULL)
+  } else { # network is up = proceed to download via curl
+    message("Downloading remote dataset.")
+    temp <- tempfile()
+    utils::download.file(data_url, temp, cacheOK = FALSE)
+    prca = utils::read.csv(unz(temp, "adv_prostate_ca/adv_prostate_ca.csv"))
+    names(prca) = toupper(names(prca))
+    return(prca)
+  }
 }
